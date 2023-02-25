07871b0f7b810b2975afea6afc5a005a8d17297d
Sebastian <1144105003@u.icesi.edu.co>
2023-02-24 23:42:25 -0500

    Back-end Template

---
+++ historial-clinico-veterinario-backend/src/test/java/com/control/veterinaria/HistorialClinicoVeterinarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.control.veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorialClinicoVeterinarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
