489423c4f69d6c6182fdf46f92a2d61f2eed7c27
Sebastian <1144105003@u.icesi.edu.co>
2023-03-02 03:53:25 -0500

    Components that will be required has been added into app module

--- historial-clinico-veterinario-frontend/src/app/app.module.ts
+++ historial-clinico-veterinario-frontend/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentsModule } from './components/components.module';
+import { PagesModule } from './pages/pages.module';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ComponentsModule,
+    PagesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- historial-clinico-veterinario-frontend/src/app/services/usuario/usuario.service.ts
+++ historial-clinico-veterinario-frontend/src/app/services/usuario/usuario.service.ts
@@ -3,13 +3,18 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioService {
-
-  private URL = "http://localhost:8080/api/usuario/";
-constructor(private http: HttpClient) { }
-public getAll(): Observable<any> { return this.http.get(this.URL) }
-public save(usuario : any):  Observable<any> { return this.http.get(this.URL, usuario) }
-public delete(id : string):  Observable<any> { return this.http.get(this.URL+id) }
+  private URL = 'http://localhost:8080/api/usuario/';
+  constructor(private http: HttpClient) {}
+  public getAll(): Observable<any> {
+    return this.http.get(this.URL);
+  }
+  public save(usuario: any): Observable<any> {
+    return this.http.get(this.URL, usuario);
+  }
+  public delete(id: string): Observable<any> {
+    return this.http.get(this.URL + id);
+  }
 }
